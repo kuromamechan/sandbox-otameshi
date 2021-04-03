8a1337da3583978615eab4d0592097c6134fa769
kuromamechan <67305485+kuromamechan@users.noreply.github.com>
2021-04-03 17:33:18 +0900

    var let constについて

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 /**
  * const,let等の変数宣言
  */
-var val1 = "var変数";
-console.log(val1);
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// //  var変数は再宣言可能
+// var val1 = "変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書き可能
+// val2 = "let変数を上書き可能";
+// console.log(val2);
+
+// let val2 = "letは再宣言できない";
+// console.log(val2);
+
+const val3 = "const変数";
+console.log(val3);
