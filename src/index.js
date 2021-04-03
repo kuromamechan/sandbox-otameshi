55feab3747ee47793dc33610bf432052fabeb1e0
kuromamechan <67305485+kuromamechan@users.noreply.github.com>
2021-04-03 21:47:47 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -74,18 +74,34 @@
 //   return str;
 // }
 // console.log(func1("funcです"));
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("fund1です"));
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("fund1です"));
 
-// アロー関数
-const func2 = (str) => {
-  return str;
-};
-console.log(func2("func2です"));
+// // アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
 
-const func3 = (num1, num2) => {
-  return num1 + num2;
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+
+const myProfile = {
+  name: "じゃけえ",
+  age: 28
 };
-console.log(func3(10, 20));
+
+const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+console.log(message1);
+
+const { name, age } = myProfile;
+const message2 = `名前は${name}です。年齢は、${age}です。`;
+console.log(message2);
