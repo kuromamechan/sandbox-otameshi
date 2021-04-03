f00b6f631690cfd0d55db5aed1e82618b2b63d90
kuromamechan <67305485+kuromamechan@users.noreply.github.com>
2021-04-03 22:02:20 +0900

    分割代入　配列も同じ

--- src/index.js
+++ src/index.js
@@ -94,14 +94,28 @@
  * 分割代入
  */
 
-const myProfile = {
-  name: "じゃけえ",
-  age: 28
-};
+// const myProfile = {
+//   name: "じゃけえ",
+//   age: 28
+// };
 
-const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
-console.log(message1);
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// console.log(message1);
 
-const { name, age } = myProfile;
-const message2 = `名前は${name}です。年齢は、${age}です。`;
-console.log(message2);
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は、${age}です。`;
+// console.log(message2);
+
+const myProfile = ["じゃけえ", 28];
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳ですね。`;
+
+console.log(message3);
+
+const [nana, aag] = myProfile;
+const message4 = `名前は${nana}です。年齢は${aag}歳ですね。`;
+
+console.log(message4);
+
+/**
+ *
+ */
